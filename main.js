307a4c7376a1417015d2eb97a62abab60d71dd5d
Arnau Davis <arnau.davis@outlook.com>
2024-12-23 23:01:17 -0500

    begun main js

---
+++ main.js
@@ -0,0 +1,21 @@
+//capturing values from form
+
+let firstName = document.querySelector('#firstName').innerText
+let lastName = document.querySelector('#lastName').innerText
+let email = document.querySelector('#email').innerText
+let phone = document.querySelector('#phone').innerText
+let street = document.querySelector('#street').innerText
+let city = document.querySelector('#city').innerText
+let state = document.querySelector('#state').innerText
+let zip = document.querySelector('#zip').innerText
+//let unhoused = document.querySelector('').innerText
+let dob = document.querySelector('#dob').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
+let  = document.querySelector('').innerText
