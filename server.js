a3b2ab70a437d5001429d561f770ff54f4c119c2
Arnau Davis <arnau.davis@outlook.com>
2024-12-25 16:38:37 -0500

    form now sends data to mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const app = express()
+const { MongoClient } = require('mongodb')
 
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 
+
+const uri = process.env.MONGODB_URI
+
+
+MongoClient.connect(uri)
+.then((client) => {
+    console.log('Connected to MongoDB')
+    const db = client.db()
+    const applicantsCollection = db.collection('applicants')
+
+    
+    app.post('/submit', (req, res) => {
+        const applicantData = req.body
+
+        applicantsCollection.insertOne(applicantData)
+            .then(() => {
+                res.send('Application submitted successfully')
+            })
+            .catch((err) => {
+                res.status(500).send('Error submitting application')
+                console.error(err)
+            });
+    });
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err)
+})
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
@@ -18,8 +47,3 @@ app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
-
-app.post('/submit',(req,res) => {
-    console.log(req.body)
-    
-})
